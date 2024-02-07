3202a08afec911f3ed7a5f519da386aab85b7540
eshaan <eshaanmandal29@gmail.com>
2024-02-07 15:33:54 +0530

    Commit #2

--- file_handling.c
+++ file_handling.c
@@ -10,8 +10,28 @@ void read_an_entire_file(char * filename) {
 
 	fclose(fptr);
 }
+
+void write_to_a_file(char * filename) {
+	FILE * fptr = fopen(filename, "w");
+
+	// we will give input until the user doesn't press -1
+
+	int input;
+	while(1) {
+		printf("Enter a number ");
+		scanf("%d", &input);
+
+		if (input == -1) 
+			break;
+		else
+			fprintf(fptr, "%d\n", input);
+	}
+
+	fclose(fptr);
+}
 int main() {
 
 	read_an_entire_file("dummy.txt");
+	write_to_a_file("newfile.txt");
 	return 0;
 }
